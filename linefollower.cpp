1db9ff70338580251e3ec32b644878b807d88408
daltonv <daltonverhagen@gmail.com>
2017-01-19 12:11:17 -0500

    First Commit

---
+++ linefollower.cpp
@@ -0,0 +1,20 @@
+/*
+ * linefollower.cpp
+ *
+ *  Created on: Jan 19, 2017
+ *      Author: dalto
+ */
+
+#include <linefollower.h>
+
+linefollower::linefollower()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+linefollower::~linefollower()
+{
+    // TODO Auto-generated destructor stub
+}
+
